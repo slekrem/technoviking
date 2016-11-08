96336b340971309708e764ddb8be3baa30077816
Stefan Merkel <merkel.s.ulm@gmail.com>
2016-11-08 22:57:39 +0100

    first test, okay ...

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 var pcap = require('pcap'),
-    pcap_session = pcap.createSession(interface, filter);
+    pcap_session = pcap.createSession("en1", "");
+
+pcap_session.on('packet', function (raw_packet) {
+  // do some stuff with a raw packet
+  console.log(raw_packet)
+});
